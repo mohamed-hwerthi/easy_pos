56bfc53d31e6a743eeb0e45c6a53729079f4d8d8
mohamed <hwerthimohamed25@gmail.com>
2025-10-30 11:02:36 +0100

    seteting some problems in the  docker compose

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,12 +3,10 @@ import { AuthResponse } from "@/models/auth-response.model";
 import axios from "axios";
 
 export const authService = {
-  // Store Owner Login
   async signIn(credentials: {
     email: string;
     password: string;
   }): Promise<AuthResponse> {
-    console.log(API_BASE_URL);
     const { data } = await axios.post(
       `${API_BASE_URL}/auth/sign-in`,
       credentials,
@@ -17,19 +15,16 @@ export const authService = {
     return data;
   },
 
-  // Logout
   async logout(): Promise<any> {
     const { data } = await apiClient.post(`${API_BASE_URL}/auth/logout`, {});
     return data;
   },
 
-  // Get Current User
   async getCurrentUser(): Promise<any> {
     const { data } = await apiClient.get(`${API_BASE_URL}/auth/current-user`);
     return data;
   },
 
-  // Token Refresh
   async refreshToken(): Promise<any> {
     const { data } = await apiClient.post(
       `${API_BASE_URL}/token/refresh-token`,
